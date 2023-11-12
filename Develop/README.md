2749af0d3980334aa66612afa06c8824ef75772d
Michaela Wells <mgwells012@gmail.com>
2023-11-12 18:10:13 -0500

    initial commit on new repository

---
+++ Develop/README.md
@@ -0,0 +1,27 @@
+# 21-Mern-Book-Search
+
+# Table of Contents
+- [Description](#description)
+- [User-Story](#user-story)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Tests](#Tests)
+- [Mock-Up](#Mock-up)
+
+# Description
+
+
+# User Story
+
+
+# Installation
+
+
+# Usage
+
+
+# Tests
+
+
+# Mock-up
+
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+// pulled from mini project
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql ' 
+type User = {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book = {
+    bookID: ID!
+    authors: [String]
+    description: [String]
+    title: [String]
+    image: [String]
+    link: [String]
+
+}
+
+type Auth = {
+    token: ID!
+    user: User
+}
+
+type Mutation = {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook
+    removeBook
+}
+
+type Query = {
+    me: User
+}
+'; 
+
+module.exports = typeDefs;
