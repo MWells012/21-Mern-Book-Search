8ecaa56ab55c28d13459ec3ed788dbc31b5ca416
Michaela Wells <mgwells012@gmail.com>
2023-11-12 18:31:48 -0500

    added the mutation code and query code to the resolvers.js file. I'm noticing that I'm getting a number of red lines on my typeDefs.js file as well, have not figured out why they're there. compared to past assignments the code should be correct

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,46 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({  _id: context.user.id}).select('-__v -password');
+                return userData
+            }
+
+            throw new AuthenticationError ('You are not logged in currently.')
+        
+        },
+    },
+    
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+          },
+       
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+
+        saveBook: {
+
+        },
+
+        removeBook: { 
+
+        },
+    },
+}
