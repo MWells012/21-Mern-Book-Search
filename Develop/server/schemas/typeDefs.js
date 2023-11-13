6286c5667cad69b6909cb4e28f85d850b7c95f10
Michaela Wells <mgwells012@gmail.com>
2023-11-12 21:21:39 -0500

    did minor de-bugging, cannot get function to run, error code is popping up in a file we do not edit

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
 // pulled from mini project
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql ' 
+const typeDefs = gql` 
 type User = {
     _id: ID!
     username: String!
@@ -35,6 +35,6 @@ type Mutation = {
 type Query = {
     me: User
 }
-'; 
+`; 
 
 module.exports = typeDefs;
